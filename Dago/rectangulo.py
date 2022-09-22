6585c106b4efd72c9be116c62d59c72bfb5f03a1
308 <308@academicos.cesde.edu.co>
2022-09-22 08:41:02 -0500

    programe el area y el perimietro del rectangulo

---
+++ Dago/rectangulo.py
@@ -0,0 +1,8 @@
+ancho = int(input("Ingresa el ancho del rectangulo: "))
+print(f'El valor del ancho del rectangulo es: {ancho}')
+largo = int(input("Ingresa el largo del rectangulo: "))
+print(f'El valor del largo del rectangulo es: {largo}')
+area = ancho * largo
+print(f'Este es el area del rectangulo: {area}')
+perimetro = (ancho+ancho+largo+largo)
+print(f'El perimetro del rectangulo es: {perimetro}')
